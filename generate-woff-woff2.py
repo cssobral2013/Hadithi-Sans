090fe71fc8ec04340fd5a56f6479f0786714ce76
Cristiano Sobral <cssobral2013@gmail.com>
2025-06-21 22:16:39 -0300

    Update v2.1

---
+++ generate-woff-woff2.py
@@ -0,0 +1,302 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/HadithiSans-Thin.ttf'
+woffPath = './web/HadithiSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/HadithiSans-ThinItalic.ttf'
+woffPath = './web/HadithiSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/HadithiSans-ExtraLight.ttf'
+woffPath = './web/HadithiSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/HadithiSans-ExtraLightItalic.ttf'
+woffPath = './web/HadithiSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/HadithiSans-Light.ttf'
+woffPath = './web/HadithiSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-LightItalic.ttf'
+woffPath = './web/HadithiSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-LightItalic.ttf'
+woffPath = './web/HadithiSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-Regular.ttf'
+woffPath = './web/HadithiSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-Italic.ttf'
+woffPath = './web/HadithiSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-Medium.ttf'
+woffPath = './web/HadithiSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-MediumItalic.ttf'
+woffPath = './web/HadithiSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBold.ttf'
+woffPath = './web/HadithiSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBoldItalic.ttf'
+woffPath = './web/HadithiSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBold.ttf'
+woffPath = './web/HadithiSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBoldItalic.ttf'
+woffPath = './web/HadithiSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-Bold.ttf'
+woffPath = './web/HadithiSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-BoldItalic.ttf'
+woffPath = './web/HadithiSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-ExtraBold.ttf'
+woffPath = './web/HadithiSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/HadithiSans-ExtraBoldItalic.ttf'
+woffPath = './web/HadithiSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/HadithiSans-Thin.ttf'
+woff2Path = './web/HadithiSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/HadithiSans-ThinItalic.ttf'
+woff2Path = './web/HadithiSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+
+srcPath  = './ttf/HadithiSans-ExtraLight.ttf'
+woff2Path = './web/HadithiSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/HadithiSans-ExtraLightItalic.ttf'
+woff2Path = './web/HadithiSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/HadithiSans-Light.ttf'
+woff2Path = './web/HadithiSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-LightItalic.ttf'
+woff2Path = './web/HadithiSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-LightItalic.ttf'
+woff2Path = './web/HadithiSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-Regular.ttf'
+woff2Path = './web/HadithiSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-Italic.ttf'
+woff2Path = './web/HadithiSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-Medium.ttf'
+woff2Path = './web/HadithiSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-MediumItalic.ttf'
+woff2Path = './web/HadithiSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBold.ttf'
+woff2Path = './web/HadithiSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBoldItalic.ttf'
+woff2Path = './web/HadithiSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBold.ttf'
+woff2Path = './web/HadithiSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-SemiBoldItalic.ttf'
+woff2Path = './web/HadithiSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-Bold.ttf'
+woff2Path = './web/HadithiSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-BoldItalic.ttf'
+woff2Path = './web/HadithiSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-ExtraBold.ttf'
+woff2Path = './web/HadithiSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/HadithiSans-ExtraBoldItalic.ttf'
+woff2Path = './web/HadithiSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
